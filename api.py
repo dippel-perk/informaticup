fdefc948531b5e39aa099e9bec850bf99b8310e8
Michael Perk <y0079915@tu-braunschweig.de>
2018-12-16 21:16:15 +0100

    Genetic Algorithm first approach

--- api.py
+++ api.py
@@ -1,15 +1,44 @@
 import requests
-import json
-
-url = 'https://phinau.de/trasi'
+import time
 
 
-def classify(file_name):
-    data = {
-        'key': 'EiCheequoobi0WuPhai3saiLud4ailep'
-    }
-    files = {
-        'image': open(file_name, 'rb')
-    }
-    resp = requests.post(url, data=data, files=files)
-    return resp.json()
+from classification import ImageClassification, Class
+
+class NeuralNetworkAPI():
+
+    DESIRED_IMAGE_EXTENSION = "PNG"
+    DESIRED_IMAGE_WIDTH = 64
+    DESIRED_IMAGE_HEIGHT = 64
+
+    __API_URL = 'https://phinau.de/trasi'
+    __API_KEY = 'EiCheequoobi0WuPhai3saiLud4ailep'
+
+    __API_RESPONSE_CODE_TOO_MANY_REQUESTS = 429
+
+    def __init__(self):
+        pass
+
+    def classify(self, file_name):
+        data = {
+            'key': NeuralNetworkAPI.__API_KEY
+        }
+        files = {
+            'image': open(file_name, 'rb')
+        }
+        resp = requests.post(NeuralNetworkAPI.__API_URL, data=data, files=files)
+
+        if resp.status_code == NeuralNetworkAPI.__API_RESPONSE_CODE_TOO_MANY_REQUESTS:
+            print("[Warning] Too many requests, waiting 60 seconds")
+            time.sleep(60)
+            return self.classify(file_name)
+
+        data = resp.json()
+
+        classes = list()
+
+        for cl in data:
+            classes.append(Class(cl["class"], cl["confidence"]))
+
+        return ImageClassification(file_name, classes)
+
+
---
+++ classification.py
@@ -0,0 +1,24 @@
+import string
+class ImageClassification:
+    def __init__(self, file, classes):
+        self.file = file
+        self.classes = classes
+
+    def share_classes(self, other):
+        own_names = (cl.name for cl in self.classes)
+        other_names = (cl.name for cl in other.classes)
+        return any(name in own_names for name in other_names)
+
+    def value_for_class(self, class_name):
+        return next((x.confidence for x in self.classes if x.name == class_name), 0)
+
+    def __repr__(self):
+        return "Image Classification %s\n%s" % (self.file, '\n'.join(cl.__repr__() for cl in self.classes))
+
+class Class:
+    def __init__(self, name : string, confidence : string):
+        self.name = name
+        self.confidence = float(confidence)
+
+    def __repr__(self):
+        return "%s %f" % (self.name, self.confidence)
